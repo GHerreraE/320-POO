72d9b38e4b22e551c80ca3d6a01b771e59a94ae2
GHerreraE <163824038+GHerreraE@users.noreply.github.com>
2024-09-17 15:40:08 +0200

    Update Exercices

---
+++ personnel/Exe_CompteBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compte_Bancaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Account cuenta = new Account(1,2,"hola");
+            cuenta.ShowAmount();
+        }
+    }
+}
